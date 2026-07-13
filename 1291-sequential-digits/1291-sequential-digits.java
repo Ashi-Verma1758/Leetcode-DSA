0cf760b8ad61c21f57579477508bfbe0c280283e
Ashi-Verma <ashiverma1758@gmail.com>
2026-07-13 11:43:32 +0530

    Time: 0 ms (100%), Space: 42 MB (94.86%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -0,0 +1,21 @@
+class Solution {
+    int[] q = new int[45];
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ans = new ArrayList<>();
+        int n =0;
+        for(int i =1; i<10; i++){
+            q[n++] =i;
+        }
+        for(int i =0; i<n; i++){
+            int d = q[i] %10;
+            if(d<9){
+                q[n++] = q[i]*10+d+1;
+            }
+        }
+        for(int x: q){
+            if(x>=low &&x<= high) ans.add(x);
+        }
+        return ans;
+        
+    }
+}
