6850ebc01b5ec9f74baa6d32facd5d60d37ae986
Ashi-Verma <ashiverma1758@gmail.com>
2026-08-01 18:46:57 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.44%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,17 @@
+class Solution {
+    Integer[][] dp;
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        dp = new Integer[n][n];
+        return solve(nums, 0, n-1)>=0;
+    }
+    int solve(int[] nums, int i, int j){
+        if(i == j) return nums[i];
+        if(dp[i][j]!= null) return dp[i][j];
+        int takleft = nums[i] - solve(nums, i+1, j);
+        int takright = nums[j] - solve(nums, i, j-1);
+        dp[i][j] = Math.max(takleft, takright);
+        return dp[i][j];
+
+    }
+}
