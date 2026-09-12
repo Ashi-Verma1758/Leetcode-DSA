becb9f3b28068c2899eadce6d1f1f437972a8999
Ashi-Verma <ashiverma1758@gmail.com>
2026-09-12 09:36:26 +0530

    Time: 6 ms (73.36%), Space: 47.2 MB (24.82%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = s.length()-1;
+        while(left<right){
+            if(arr[left] > arr[right]){
+                arr[left] = arr[right];
+            }else{
+                arr[right] = arr[left];
+            }
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+}
